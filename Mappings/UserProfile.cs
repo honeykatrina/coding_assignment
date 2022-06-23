cd06f7628a7738aed8869044f29b137056590338
Kateryna Morozova <kateryna.morozova@viseo.com>
2022-06-23 20:25:51 +0200

    Update user response model

--- Mappings/UserProfile.cs
+++ Mappings/UserProfile.cs
@@ -10,7 +10,9 @@ public class UserProfile : Profile
     public UserProfile()
     {
         CreateMap<User, UserResponseModel>()
-            .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.Account.Balance));
+            .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.Account.Balance))
+            .ForMember(dest => dest.AccountId, opt => opt.MapFrom(src => src.Account.Id))
+            .ForMember(dest => dest.CustomerId, opt => opt.MapFrom(src => src.Account.CustomerId));
 
         CreateMap<CreateUserRequest, User>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
