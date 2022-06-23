3b765302d9971f7401993dc2aceb719ddabaed9c
Kateryna Morozova <kateryna.morozova@viseo.com>
2022-06-23 23:14:08 +0200

    Extract data files

--- TransactionModule/Repositories/TransactionRepository.cs
+++ TransactionModule/Repositories/TransactionRepository.cs
@@ -5,18 +5,20 @@ namespace UserAccountManagement.TransactionModule.Repositories;
 
 public class TransactionRepository : ITransactionRepository
 {
+    private const string Path = "Data\\Transactions.json";
+
     public void Create(Transaction transaction)
     {
-        var jsonString = File.ReadAllText("Transactions.json");
+        var jsonString = File.ReadAllText(Path);
         var transactions = JsonSerializer.Deserialize<List<Transaction>>(jsonString);
         transactions.Add(transaction);
-        using var file = File.CreateText("Transactions.json");
+        using var file = File.CreateText(Path);
         file.WriteLine(JsonSerializer.Serialize(transactions));
     }
 
     public List<Transaction> GetByAccountId(Guid accountId)
     {
-        var jsonString = File.ReadAllText("Transactions.json");
+        var jsonString = File.ReadAllText(Path);
         var transactions = JsonSerializer.Deserialize<List<Transaction>>(jsonString);
         return transactions.Where(t => t.AccountId == accountId).ToList();
     }
--- UserModule/Repositories/UserRepository.cs
+++ UserModule/Repositories/UserRepository.cs
@@ -5,33 +5,34 @@ namespace UserAccountManagement.UserModule.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private const string Path = "Data\\Users.json";
     public User GetByCustomerId(int customerId)
     {
-        var jsonString = File.ReadAllText("Users.json");
+        var jsonString = File.ReadAllText(Path);
         var users = JsonSerializer.Deserialize<List<User>>(jsonString);
         return users.FirstOrDefault(x => x.Account.CustomerId == customerId);
     }
 
     public User GetById(Guid id)
     {
-        var jsonString = File.ReadAllText("Users.json");
+        var jsonString = File.ReadAllText(Path);
         var users = JsonSerializer.Deserialize<List<User>>(jsonString);
         return users.FirstOrDefault(x => x.Id == id);
     }
 
     public List<User> GetAll()
     {
-        var jsonString = File.ReadAllText("Users.json");
+        var jsonString = File.ReadAllText(Path);
         var users = JsonSerializer.Deserialize<List<User>>(jsonString);
         return users;
     }
 
     public void Create(User user)
     {
-        var jsonString = File.ReadAllText("Users.json");
+        var jsonString = File.ReadAllText(Path);
         var users = JsonSerializer.Deserialize<List<User>>(jsonString);
         users.Add(user);
-        using var file = File.CreateText("Users.json");
+        using var file = File.CreateText(Path);
         file.WriteLine(JsonSerializer.Serialize(users));
     }
 }
