b6806dd60143229828b64c310f45a28ee08426ca
Kateryna Morozova <kateryna.morozova@viseo.com>
2022-06-24 13:51:35 +0200

    Remove duplicated files

--- Models/Responses/TransactionResponseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserAccountManagement.Models.Responses;
-
-public class TransactionResponseModel
-{
-    public double Amount { get; set; }
-
-    public DateTimeOffset CreationDate { get; set; }
-}
