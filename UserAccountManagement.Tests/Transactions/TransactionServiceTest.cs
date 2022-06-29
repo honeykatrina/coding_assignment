a994bfa7a57add6e79c88f07efce630a52e0d4fc
Kateryna Morozova <kateryna.morozova@viseo.com>
2022-06-30 01:01:21 +0200

    Add test project with unit tests

---
+++ UserAccountManagement.Tests/Transactions/TransactionServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using UserAccountManagement.TransactionModule.Models.Entities;
+using UserAccountManagement.TransactionModule.Models.Responses;
+using UserAccountManagement.TransactionModule.Repositories;
+using UserAccountManagement.TransactionModule.Services;
+
+namespace UserAccountManagement.Tests.Transactions;
+
+public class TransactionServiceTest
+{
+    [Fact]
+    public void GetTransactionsShouldReturnListOfTransactions()
+    {
+        var transactions = GetTransactions();
+        var transactionsResponse = GetTransactionsResponse();
+
+        var transactionRepositoryMock = new Mock<ITransactionRepository>();
+        transactionRepositoryMock.Setup(m => m.GetAll()).Returns(transactions);
+        var mapperMock = new Mock<IMapper>();
+        mapperMock
+            .Setup(m => m.Map<List<TransactionResponseModel>>(transactions))
+            .Returns(transactionsResponse);
+        var transactionService = new TransactionService(
+            transactionRepositoryMock.Object,
+            mapperMock.Object);
+        var actualTransactions = transactionService.GetTransactions();
+
+        actualTransactions.Success.Should().BeTrue();
+        actualTransactions.Error.Should().BeNull();
+        actualTransactions.Model.Should().HaveCount(2);
+        actualTransactions.Model.Should().BeEquivalentTo(transactionsResponse);
+    }
+
+    private static List<TransactionResponseModel> GetTransactionsResponse()
+    {
+        return new List<TransactionResponseModel>()
+        {
+            new TransactionResponseModel()
+            {
+                AccountId = Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                Amount = 200,
+                CreationDate = DateTime.Parse("2021-09-15T00:00:00+02:00")
+            },
+            new TransactionResponseModel()
+            {
+                AccountId = Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                Amount = 100,
+                CreationDate = DateTime.Parse("2021-07-26T00:00:00+02:00")
+            }
+        };
+    }
+
+    private static List<Transaction> GetTransactions()
+    {
+        return new List<Transaction>()
+        {
+            new Transaction()
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                Amount = 200,
+                CreationDate = DateTime.Parse("2021-09-15T00:00:00+02:00")
+            },
+            new Transaction()
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                Amount = 100,
+                CreationDate = DateTime.Parse("2021-07-26T00:00:00+02:00")
+            }
+        };
+    }
+}
---
+++ UserAccountManagement.Tests/Users/UserServiceTest.cs
@@ -0,0 +1,70 @@
+﻿using UserAccountManagement.TransactionModule.Repositories;
+using UserAccountManagement.UserModule.Models.Entities;
+using UserAccountManagement.UserModule.Models.Responses;
+using UserAccountManagement.UserModule.Repositories;
+using UserAccountManagement.UserModule.Services;
+
+namespace UserAccountManagement.Tests.Users;
+
+public class UserServiceTest
+{
+    [Fact]
+    public void GetUsersShouldReturnListOfUsers()
+    {
+        var users = GetUsers();
+        var usersResponse = GetUsersResponse();
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+        userRepositoryMock
+            .Setup(m => m.GetAll())
+            .Returns(users);
+        var transactionRepositoryMock = new Mock<ITransactionRepository>();
+        var mapperMock = new Mock<IMapper>();
+        mapperMock
+            .Setup(m => m.Map<List<UserResponseModel>>(users))
+            .Returns(usersResponse);
+        var userService = new UserService(
+            userRepositoryMock.Object,
+            transactionRepositoryMock.Object,
+            mapperMock.Object);
+        var actualUsers = userService.GetUsers();
+
+        actualUsers.Success.Should().BeTrue();
+        actualUsers.Error.Should().BeNull();
+        actualUsers.Model.Should().HaveCount(1);
+        actualUsers.Model.Should().BeEquivalentTo(usersResponse);
+    }
+
+    private static List<UserResponseModel> GetUsersResponse()
+    {
+        return new List<UserResponseModel>()
+        {
+            new UserResponseModel()
+            {
+                Name="Kate",
+                Surname="Morozova",
+                AccountId= Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                CustomerId=1,
+                Balance=300
+            }
+        };
+    }
+
+    private static List<User> GetUsers()
+    {
+        return new List<User>()
+        {
+            new User()
+            {
+                Id = Guid.NewGuid(),
+                Name="Kate",
+                Surname="Morozova",
+                Account= new Account {
+                    Id= Guid.Parse("1b61a218-84f3-411a-8238-e9c3196fd387"),
+                    CustomerId=1,
+                    Balance=300
+                }
+            }
+        };
+    }
+}
